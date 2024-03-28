097b08f7af422b4c35c8ab1f37e8fbba622a561c
saras56 <sarasteffy56@gmail.com>
2024-03-28 10:56:03 +0530

    new update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 # import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
+pipe = pd.read_pickle("pipe.pkl")
+df = pd.read_pickle("df.pkl")
+
+st.title("Laptop Price Predictor")
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
